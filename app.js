91232692280abb45ac12bd287e5e35e5e7215e83
nanorsorpor1 <natipong_na@hotmail.com>
2021-12-29 22:35:17 +0700

    add all file
    hello world

---
+++ app.js
@@ -0,0 +1,16 @@
+import express from 'express';
+import chalk from 'chalk';
+import debug from 'debug';
+import morgan from 'morgan';
+const app = express();
+const port = 3000;
+
+app.use(morgan('combined'));
+
+app.get('/',(req,res)=>{
+    res.send('hello nay')
+})
+
+app.listen(port,()=>{
+    debug('listening on port'+ chalk.red(port))
+})
